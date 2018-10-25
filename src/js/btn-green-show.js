81f886fda6c5b3f2bfbe25959e48eafef2938976
yaji2k <yaji2k@gmail.com>
2018-10-25 17:58:33 +0300

    edited

--- src/js/btn-green-show.js
+++ src/js/btn-green-show.js
@@ -1,9 +1,14 @@
 $(document).ready(function () {
-    setTimeout(displayBtn, 3000);
+    // setTimeout(displayBtn, 3000);
+    animate3D();
 
     function displayBtn() {
         $(".phone_btn_green").css({"display": "block"}).animate({
             opacity: 1
         }, 700);
+    }
+    function animate3D() {
+        var list = $(".phone_btn_green .");
+        console.log(list.length);
     }
 });
