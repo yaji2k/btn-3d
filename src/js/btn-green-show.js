e4e33c31b32ca6d9d54b1e73e6b2572e21135911
yaji2k <yaji2k@gmail.com>
2018-10-27 17:54:39 +0300

    added 2 button

--- src/js/btn-green-show.js
+++ src/js/btn-green-show.js
@@ -1,14 +1,74 @@
 $(document).ready(function () {
-    // setTimeout(displayBtn, 3000);
-    animate3D();
+    animateCube({el: ".cube_container", parent: ".cube_side"});
 
-    function displayBtn() {
-        $(".phone_btn_green").css({"display": "block"}).animate({
-            opacity: 1
-        }, 700);
-    }
-    function animate3D() {
-        var list = $(".phone_btn_green .");
-        console.log(list.length);
+    function animateCube(obj) {
+        var selector = obj.el || "";
+        var parent = obj.parent || "";
+        var color = obj.color || "#67de99";
+        var shadowColor = obj.shadowColor || "#54cb84";
+        var value = obj.rotate || -90;
+        var vector = true;
+        var isMove = true;
+        var timeout = obj.timeout || 3000;
+        var parentIndex = 0;
+        var cont = $(selector);
+        var parents = $(cont).find(parent);
+
+        recursiveMove();
+
+        cont.on("mousemove", function () {
+            isMove = false;
+            while (value < 90) {
+                value += 90;
+                parentIndex++;
+                editPosition();
+            }
+        });
+        cont.on("mouseleave", function () {
+            isMove = true;
+            checkVector();
+            editPosition();
+        });
+
+        function editPosition() {
+            $(parents[parentIndex]).css({background: color});
+            $(parents[parentIndex + 1]).css({background: shadowColor});
+            cont.css({transform: "rotateX(" + value + "deg)"});
+
+        }
+
+        function recursiveMove() {
+            setTimeout(function () {
+                if (isMove) {
+                    move();
+                }
+                recursiveMove();
+            }, timeout);
+        }
+
+        function move() {
+            checkVector();
+            editCount();
+            editPosition();
+        }
+
+        function checkVector() {
+            if (value == 90) {
+                vector = false;
+            }
+            if (value == -90) {
+                vector = true;
+            }
+        }
+
+        function editCount() {
+            if (vector) {
+                value += 90;
+                parentIndex++;
+            } else {
+                value -= 90;
+                parentIndex--;
+            }
+        }
     }
 });
--- src/js/test.js
+++ src/js/test.js
@@ -1,35 +1,78 @@
 $(document).ready(function () {
+    animateCube({el: ".cube_container", parent: ".cube_side"});
 
-    tick();
+    function animateCube(obj) {
+        var selector = obj.el || "";
+        var parent = obj.parent || "";
+        var color = obj.color || "#67de99";
+        var shadowColor = obj.shadowColor || "#54cb84";
+        var value = obj.rotate || -90;
+        var vector = true;
+        var isMove = true;
+        var timeout = obj.timeout || 3000;
+        var parentIndex = 0;
+        var cont = $(selector);
+        var parents = $(cont).find(parent);
 
-    function tick() {
-        var cont = $(".container");
-        var count = 0;
-        var active = true;
-        go();
+        recursiveMove();
+
         cont.on("mousemove", function () {
-            active = false;
+            isMove = false;
+            while(value < 90) {
+                value += 90;
+                parentIndex++;
+                editPosition();
+            }
         });
         cont.on("mouseleave", function () {
-            active = true;
-            go();
+            isMove = true;
+            checkVector();
+            editPosition();
         });
 
-        function go() {
-            if (active) {
-                setTimeout(function () {
-                    $(cont).animate({
-                        textIndent: 0
-                    }, {
-                        step: function (count, fx) {
-                            $(this).css('-webkit-transform', 'rotate(' + count + 'deg)');
-                            $(this).css('transform', 'rotate(' + count + 'deg)');
-                        },
-                    }, 1000);
-                    count += 90;
-                    go();
-                }, 2000);
+        function editPosition() {
+            $(parents[parentIndex]).css({background: color});
+            $(parents[parentIndex+1]).css({background: shadowColor});
+            cont.css({transform: "rotateX(" + value + "deg)"});
+
+        }
+
+        function recursiveMove() {
+            setTimeout(function () {
+                if (isMove) {
+                    move();
+                }
+                recursiveMove();
+            }, timeout);
+        }
+
+        function move() {
+            checkVector();
+            editCount();
+            editPosition();
+        }
+
+        function checkVector() {
+            if (value == 90) {
+                vector = false;
+            }
+            if (value == -90) {
+                vector = true;
+            }
+        }
+
+        function editCount() {
+            if (vector) {
+                value += 90;
+                parentIndex++;
+            } else {
+                value -= 90;
+                parentIndex--;
             }
         }
     }
 });
+
+
+
+
